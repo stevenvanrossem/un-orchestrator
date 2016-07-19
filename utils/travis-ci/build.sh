e0f8d9a2e9c21f2d0a1aaee448e2e924f20027a0
Mauricio Vasquez B <mauricio.vasquezbernal@studenti.polito.it>
2016-07-19 16:38:20 +0200

    travis-ci: fix build with new building system
    The new building system does not allow to compile the name resolver or the
    orchestrator as single elements. All of them have to be compiled using the
    cmake that is present in the root folder.

--- utils/travis-ci/build.sh
+++ utils/travis-ci/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-BASE=`pwd`
+set -ev # print every line before executing it and exit if one command fails
 
-# build name resolver
-cd $BASE/name-resolver
-cmake .
-make -j2
+# build name resolver and orchestrator
+cmake -DCMAKE_BUILD_TYPE=Release \
+ -DENABLE_KVM=$KVM -DENABLE_DOCKER=$DOCKER \
+ -DENABLE_NATIVE=$NATIVE \
+ -DENABLE_DPDK_PROCESSES=$DPDK \
+ -DVSWITCH_IMPLEMENTATION=$VSWITCH \
+ -DENABLE_DOUBLE_DECKER_CONNECTION=$DD \
+ -DENABLE_RESOURCE_MANAGER=$DD \
+ -DBUILD_ExternalProjects=OFF .
 
-# build orchestrator
-cd $BASE/orchestrator
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_KVM=$KVM -DENABLE_DOCKER=$DOCKER -DENABLE_NATIVE=$NATIVE -DENABLE_DPDK_PROCESSES=$DPDK -DVSWITCH_IMPLEMENTATION=$VSWITCH -DENABLE_DOUBLE_DECKER_CONNECTION=$DD -DENABLE_RESOURCE_MANAGER=$DD .
 make -j2
