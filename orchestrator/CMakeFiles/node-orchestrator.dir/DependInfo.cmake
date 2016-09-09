924339247d468c922608eb9eee6ae3c17d56465d
Steven Van Rossem <steven.vanrossem@intec.ugent.be>
2016-09-09 09:37:43 +0200

    merge orch-stable updates

---
+++ orchestrator/CMakeFiles/node-orchestrator.dir/DependInfo.cmake
@@ -0,0 +1,81 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/utils/logger.c" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/utils/logger.c.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/utils/sockutils.c" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/utils/sockutils.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/compute_controller/compute_controller.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/compute_controller/compute_controller.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/compute_controller/description.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/compute_controller/description.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/compute_controller/nf.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/compute_controller/nf.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/compute_controller/nfs_manager.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/compute_controller/nfs_manager.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/compute_controller/plugins/docker/docker.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/compute_controller/plugins/docker/docker.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/monitoring_controller/monitoring_controller.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/monitoring_controller/monitoring_controller.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/network_controller/openflow_controller/controller.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/network_controller/openflow_controller/controller.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/commands.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/network_controller/switch_manager/plugins/ovs-ovsdb/commands.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/ovsdb_manager.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/network_controller/switch_manager/plugins/ovs-ovsdb/ovsdb_manager.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_orchestrator.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_orchestrator.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/database_manager/SQLite/SQLiteManager.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/database_manager/SQLite/SQLiteManager.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/generic_actions/generic_action.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/generic_actions/generic_action.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/generic_actions/vlan_action.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/generic_actions/vlan_action.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/graph-parser/address_validator.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/graph-parser/address_validator.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/graph-parser/graph_parser.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/graph-parser/graph_parser.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/graph-parser/match_parser.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/graph-parser/match_parser.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_graph.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_graph.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_gre.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_gre.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_interface.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_interface.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_internal.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_internal.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_vlan.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_graph_endpoint_vlan.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_graph_vnf.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_graph_vnf.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_match.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_match.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_output_action.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_output_action.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_output_action_endpoint_gre.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_output_action_endpoint_gre.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_output_action_endpoint_internal.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_output_action_endpoint_internal.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_output_action_nf.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_output_action_nf.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_output_action_port.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_output_action_port.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/high_level_graph/high_level_rule.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/high_level_graph/high_level_rule.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/low_level_graph/graph.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/low_level_graph/graph.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/low_level_graph/low_level_match.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/low_level_graph/low_level_match.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/low_level_graph/output_action.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/low_level_graph/output_action.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/low_level_graph/rule.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/low_level_graph/rule.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph/match.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph/match.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph_manager/graph_info.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph_manager/graph_info.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph_manager/graph_manager.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph_manager/graph_manager.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph_manager/graph_translator.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph_manager/graph_translator.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph_manager/lsi.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph_manager/lsi.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/graph_manager/virtual_link.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/graph_manager/virtual_link.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/pub_sub/pub_sub.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/pub_sub/pub_sub.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/rest_server/rest_server.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/rest_server/rest_server.cc.o"
+  "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/node_resource_manager/security_manager/security_manager.cc" "/home/steven/Documents/UNIFY/un-orchestrator_new_elastic_router/un-orchestrator/orchestrator/CMakeFiles/node-orchestrator.dir/node_resource_manager/security_manager/security_manager.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ENABLE_DOCKER"
+  "ENABLE_DOUBLE_DECKER_CONNECTION"
+  "ENABLE_UNIFY_MONITORING_CONTROLLER"
+  "ENABLE_UNIFY_PORTS_CONFIGURATION"
+  "LOGGING_LEVEL=ORCH_DEBUG_INFO"
+  "RUN_NFS"
+  "VSWITCH_IMPLEMENTATION_OVSDB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "externals/include"
+  "/usr/include/boost"
+  "/usr/include/libxml2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
